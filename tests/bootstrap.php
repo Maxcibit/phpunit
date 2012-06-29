a3eaef327ca8f60f0a203173cd73c8f711db4831
Maximilian Röhrle <maximilian.roehrle@staudigl-druck.de>
2012-06-29 10:06:06 +0200

    bugfix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -28,36 +28,4 @@ if ('@package_version@' !== $phpUnitVersion && version_compare($phpUnitVersion, 
 }
 unset($phpUnitVersion);
 
-/** 
- * Daten der Anwendung laden
- */
-require_once('../inc/Data.php');
-require_once('../inc/ArrayFactory.php');
-require_once('../inc/functions.php');
-
-/**
- * Erstelle TestResult-Objekt und den Observer zum Debuggen
- * der Testausführung.
- */
-$result = new PHPUnit_Framework_TestResult;
-
-require_once('TestDebugger.php');
-$result->addListener(new TestDebugger);
-
-require_once('testsuites/ArrayTestSuite.php');
-$suite = new PHPUnit_Framework_TestSuite('class ArrayTest');
-$suite->addTest(new ArrayTest('testNewArrayIsEmpty'));
-$suite->addTest(new ArrayTest('testArrayContainsAnElement'));
-$suite->run($result);
-
-require_once('testsuites/DataTestSuite.php');
-$suite = new PHPUnit_Framework_TestSuite('class DataTest');
-$suite->addTest(new DataTest('testAdd'));
-$suite->run($result);
-
-require_once('testsuites/GeneralTestSuite.php');
-$suite = new PHPUnit_Framework_TestSuite('class GeneralTest');
-$suite->addTest(new GeneralTest('testFunctions'));
-$suite->run($result);
-
 ?>
