f0b4447bd2858383ff9f4a33494fab8648de4c22
Maximilian Röhrle <maximilian.roehrle@staudigl-druck.de>
2012-06-29 11:16:28 +0200

    bugfix

---
+++ index_clone.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('inc/functions.php');
+require_once('inc/Data.php');
+
+var_dump(Data::get());
+
+$test = "abc";
+$test .= addSampleText();
+$test .= addSampleText2();
+
+$doTheSame = 1;
+$doTheSame = 1;
+$doTheSame = 1;
+$doTheSame = 1;
+
+require_once('inc/ArrayFactory.php');
+$array = ArrayFactory::createEmptyArray();
+
+?>
