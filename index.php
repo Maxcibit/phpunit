211fdaa83fa18855acc832e93837738fbf397d51
Maximilian Röhrle <maximilian.roehrle@staudigl-druck.de>
2012-06-29 08:24:13 +0200

    using more bp-tools like phpcpd

--- index.php
+++ index.php
@@ -4,6 +4,19 @@
 require_once('inc/Data.php');
 var_dump(Data::get());
 
+/**
+ * sample for cpd
+ */
+function addSampleText() {
+	return "def";	
+}
+function addSampleText2() {
+	return "def";	
+}
+$test = "abc";
+$test .= addSampleText();
+$test .= addSampleText2();
+
 require_once('inc/ArrayFactory.php');
 $array = ArrayFactory::createEmptyArray();
 
